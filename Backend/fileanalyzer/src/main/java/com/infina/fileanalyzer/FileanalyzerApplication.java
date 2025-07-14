607cb60c7057486b8e3ecf81941aa79681f7d261
umutsefkansak <umut.sefkan4@gmail.com>
2025-07-14 18:32:53 +0300

    Created initial project structure

---
+++ Backend/fileanalyzer/src/main/java/com/infina/fileanalyzer/FileanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.infina.fileanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileanalyzerApplication.class, args);
+	}
+
+}
---
+++ Backend/fileanalyzer/src/test/java/com/infina/fileanalyzer/FileanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infina.fileanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
